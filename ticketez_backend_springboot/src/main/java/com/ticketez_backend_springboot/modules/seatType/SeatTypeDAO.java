fe911b65b64eaab7ea080cebbe7947ba3ce758d4
HuuTaiOne <nguyentaint979@gmail.com>
2023-10-13 20:14:38 +0700

    [FE, BE], Hữu Tài - viết api SeatType và hoàn thành SeatType

--- ticketez_backend_springboot/src/main/java/com/ticketez_backend_springboot/modules/seatType/SeatTypeDAO.java
+++ ticketez_backend_springboot/src/main/java/com/ticketez_backend_springboot/modules/seatType/SeatTypeDAO.java
@@ -1,7 +1,12 @@
 package com.ticketez_backend_springboot.modules.seatType;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface SeatTypeDAO extends JpaRepository<SeatType, Long> {
+
+    List<SeatType> findAllByOrderByIdDesc();
 
 }
