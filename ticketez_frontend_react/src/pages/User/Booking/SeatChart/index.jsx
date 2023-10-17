42c9c859455b20acf0fa929024eed28d01336459
Phùng Tự Hiếu <134354413+phungtuhieu@users.noreply.github.com>
2023-10-17 09:09:08 +0000

    [Merge - tuhieu]

--- ticketez_frontend_react/src/pages/User/Booking/SeatChart/index.jsx
+++ ticketez_frontend_react/src/pages/User/Booking/SeatChart/index.jsx
@@ -9,27 +9,11 @@ import funcUtils from '~/utils/funcUtils';
 import { BookingDetail } from '../..';
 import { DndProvider, useDrag, useDrop } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-import { CloseOutlined } from '@ant-design/icons';
 
 const cx = classNames.bind(style);
 function SeatChart(props) {
     const { rows, columns, idSeatChart } = props;
-    // Hiện modal thông tin ghế
-    const [isModalOpenSeat, setIsModalOpenSeat] = useState(false);
 
-    const showModaSeat = () => {
-        setIsModalOpenSeat(true);
-    };
-
-    const handleOkSeat = () => {
-        setIsModalOpenSeat(false);
-    };
-
-    const handleCancelSeat = () => {
-        setIsModalOpenSeat(false);
-    };
-
-    // Hiện modal đặt qué
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const showModal = () => {
@@ -205,37 +189,22 @@ function SeatChart(props) {
     useEffect(() => {
         fetchDataSeat();
     }, [reload]);
-
-    // Phóng to thu nhỏ
     const [tableScale, setTableScale] = useState(1);
 
+    const handleZoomIn = () => {
+        setTableScale(tableScale + 0.1);
+    };
 
-    const handleWheel = (event) => {
-        if (event.deltaY > 0) {
-            // Cuộn xuống (thu nhỏ)
-            if (tableScale > 1) {
-                setTableScale(tableScale - 0.1);
-            }
-        } else {
-            // Cuộn lên (phóng to)
-            setTableScale(tableScale + 0.1);
+    const handleZoomOut = () => {
+        if (tableScale > 1) {
+            setTableScale(tableScale - 0.1);
         }
     };
 
-    useEffect(() => {
-        // Đăng ký sự kiện wheel khi component được mount
-        window.addEventListener('wheel', handleWheel);
-
-        // Hủy đăng ký sự kiện khi component unmount
-        return () => {
-            window.removeEventListener('wheel', handleWheel);
-        };
-    }, [tableScale]);
-
     return (
         <>
-            {/* <button onClick={handleZoomIn}>Phóng to</button>
-            <button onClick={handleZoomOut}>Thu nhỏ</button> */}
+            <button onClick={handleZoomIn}>Phóng to</button>
+            <button onClick={handleZoomOut}>Thu nhỏ</button>
 
             <Card className="card" style={{ display: 'flex' }}>
                 <Row className="card">
@@ -256,7 +225,7 @@ function SeatChart(props) {
                                 <div style={{ marginBottom: '200px', backgroundColor: 'red' }}></div>
 
                                 <table
-                                    className="grid zoom-container "
+                                    className="grid "
                                     style={{ maxWidth: '100%', maxHeight: '100%', transform: `scale(${tableScale})` }}
                                 >
                                     {showSeat && (
@@ -324,8 +293,11 @@ function SeatChart(props) {
                         </div>
                     </DndProvider>
                 </Row>
+
+                <hr></hr>
                 <Row gutter={10}>
                     <Col span={24} style={{ display: 'flex', justifyContent: 'center' }}>
+                        <hr></hr>
                         <div style={{ padding: '10px' }}>
                             <Space size={[0, 200]} wrap>
                                 <Tag className={cx('tagg')} color="#404040">
@@ -343,211 +315,54 @@ function SeatChart(props) {
                             </Space>
                         </div>
                     </Col>
-                    <Col span={24} style={{ display: 'flex', justifyContent: 'center' }}>
-                        <div style={{ color: 'white', display: 'flex', alignItems: 'center' }}>
-                            <div
-                                onClick={showModaSeat}
-                                style={{ cursor: 'pointer', textDecoration: 'underline', fontWeight: 'bolder' }}
-                            >
-                                Xem chi tiết hình
+                    <Col
+                        span={100}
+                        xs={24}
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'flex-end',
+                            paddingTop: '50px',
+                        }}
+                    >
+                        <Col span={24} style={{ display: 'flex', justifyContent: 'flex-start' }}>
+                            <div style={{ padding: '10px', color: 'white' }}>
+                                <h3>Ghế bạn đã chọn:</h3>
+                                {seatState && seatState.seatReserved && seatState.seatReserved.length > 0 ? (
+                                    seatState.seatReserved.map((seat, index) => <p key={index}>{seat}</p>)
+                                ) : (
+                                    <p>Không có ghế nào được chọn.</p>
+                                )}
                             </div>
-                            <p style={{ marginLeft: '10px' }}>ảnh và thông tin ghế</p>
-                        </div>
+                        </Col>
+                        <Row>
+                            <Col span={24} style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                                <div>
+                                    <Button
+                                        style={{
+                                            width: '200px',
+                                            height: '70px',
+                                            backgroundColor: '#EB2F96',
+                                            fontWeight: 'bolder',
+                                        }}
+                                        className={cx('btn')}
+                                        type="primary"
+                                        onClick={showModal}
+                                        icon={
+                                            <ShoppingOutlined
+                                                style={{ fontSize: '32px' }} // Đổi kích thước và màu sắc
+                                            />
+                                        }
+                                    >
+                                        Mua vé
+                                    </Button>
+                                </div>
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
-                <div style={{ backgroundColor: 'white', padding: '20px', borderRadius: '20px' }}>
-                    <Row>
-                        <Col span={24} style={{ display: 'flex', justifyContent: 'flex-start' }}>
-                            <h2 style={{ fontWeight: 'bolder' }}>Phim: Đất rừng phương nam</h2>
-                        </Col>
-                        <Col span={24} style={{ display: 'flex', justifyContent: 'flex-start' }}>
-                            <h3 style={{ color: 'orange', fontFamily: '-moz-initial' }}>
-                                15:45 ~ 17:35 Hôm nay , 16/10 phòng chiếu Screen 02 - 2D lồng tiếng
-                            </h3>
-                        </Col>
-                    </Row>
-                    <hr></hr>
-                    <Row>
-                        <Col
-                            span={100}
-                            xs={24}
-                            style={{
-                                paddingTop: '50px',
-                            }}
-                        >
-                            <Col span={24} style={{ marginBottom: '20px' }}>
-                                <Col>
-                                    <Row style={{ color: 'black' }}>
-                                        {' '}
-                                        <h3 style={{ float: 'left', marginRight: '10px', marginTop: '5px' }}>
-                                            Ghế bạn đã chọn:
-                                        </h3>
-                                        <div
-                                            style={{
-                                                display: 'flex',
-                                                alignItems: 'center',
-                                                border: '1px solid rgba(169, 169, 169, 0.3)',
-
-                                                borderRadius: '10px',
-                                                padding: '3px',
-                                                display:
-                                                    seatState &&
-                                                    seatState.seatReserved &&
-                                                    seatState.seatReserved.length > 0
-                                                        ? 'flex'
-                                                        : 'none',
-                                            }}
-                                        >
-                                            {' '}
-                                            <div
-                                                className="hoverDiv"
-                                                style={{
-                                                    marginRight: '10px',
-                                                    padding: '3px',
-                                                    cursor: 'pointer',
-                                                }}
-                                            >
-                                                <CloseOutlined
-                                                    style={{
-                                                        color: 'red',
-                                                        fontSize: '20px',
-                                                        fontWeight: 'bold',
-                                                        transition: 'color 0.3s',
-                                                    }}
-                                                />
-                                            </div>
-                                            {seatState &&
-                                            seatState.seatReserved &&
-                                            seatState.seatReserved.length > 0 ? (
-                                                seatState.seatReserved.map((seat, index) => (
-                                                    <p style={{ fontSize: '20px' }} key={index}>
-                                                        {index === 0 ? seat : `, ${seat}`}
-                                                    </p>
-                                                ))
-                                            ) : (
-                                                <p></p>
-                                            )}
-                                        </div>
-                                    </Row>
-                                </Col>
-                            </Col>
-                            <hr></hr>
-                            <Row style={{ marginTop: '20px' }}>
-                                <Col>
-                                    {' '}
-                                    <div
-                                        style={{
-                                            display: 'flex',
-                                            alignItems: 'center',
-                                        }}
-                                    >
-                                        {' '}
-                                        <p
-                                            style={{
-                                                float: 'left',
-                                                marginRight: '10px',
-                                                fontSize: '20px',
-                                                color: 'silver',
-                                            }}
-                                        >
-                                            Tạm tính:
-                                        </p>
-                                        <p
-                                            style={{
-                                                float: 'left',
-
-                                                fontSize: '20px',
-                                                fontWeight: 'bolder',
-                                            }}
-                                        >
-                                            200000Đ
-                                        </p>
-                                    </div>
-                                </Col>
-                                <Col span={24} style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                                    <div>
-                                        <Button
-                                            style={{
-                                                width: '200px',
-                                                height: '70px',
-                                                backgroundColor: '#EB2F96',
-                                                fontWeight: 'bolder',
-                                            }}
-                                            className={cx('btn')}
-                                            type="primary"
-                                            onClick={showModal}
-                                            icon={
-                                                <ShoppingOutlined
-                                                    style={{ fontSize: '32px' }} // Đổi kích thước và màu sắc
-                                                />
-                                            }
-                                        >
-                                            Mua vé
-                                        </Button>
-                                    </div>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-                </div>
             </Card>
-            <Modal
-                title="Chi tiết loại ghế"
-                open={isModalOpenSeat}
-                onOk={handleOkSeat}
-                onCancel={handleCancelSeat}
-                footer={null}
-            >
-                <Card
-                    style={{
-                        background: `url('https://homepage.momocdn.net/cinema/momo-upload-api-221122145441-638047256815541651.jpg')`,
-                        backgroundSize: '100%',
-                        backgroundPosition: 'center',
-                        backgroundRepeat: 'no-repeat',
-                    }}
-                >
-                    <div style={{ marginTop: '200px', color: 'white' }}>
-                        <h3 style={{ fontWeight: 'bolder' }}>Ghế thường</h3>
-                        <p>Ghế đệm nhung êm ái, chỗ để tay thoải mái.</p>
-                    </div>
-                </Card>
-
-                <Card
-                    style={{
-                        background: `url('https://homepage.momocdn.net/cinema/momo-upload-api-221122145441-638047256815541651.jpg')`,
-                        backgroundSize: '100%',
-                        backgroundPosition: 'center',
-                        backgroundRepeat: 'no-repeat',
-                    }}
-                >
-                    <div style={{ marginTop: '200px', color: 'white' }}>
-                        <h3 style={{ fontWeight: 'bolder' }}>Ghế Vip</h3>
-                        <p>
-                            Ghế đệm nhung êm ái, chỗ để tay thoải mái. Vị trí giữa màn hình để có trải nghiệm điện ảnh
-                            hoàn thiện nhất
-                        </p>
-                    </div>
-                </Card>
-
-                <Card
-                    style={{
-                        background: `url('https://homepage.momocdn.net/cinema/momo-upload-api-221122145649-638047258093509168.jpg')`,
-                        backgroundSize: '100%',
-                        backgroundPosition: 'center',
-                        backgroundRepeat: 'no-repeat',
-                    }}
-                >
-                    <div style={{ marginTop: '200px', color: 'white' }}>
-                        <h3 style={{ fontWeight: 'bolder' }}>Ghế Đôi</h3>
-                        <p>
-                            Ghế đệm nhung êm ái, chỗ để tay thoải mái. Không gian riêng tư, thoải mái cho các cặp đôi
-                            hoặc bạn bè.
-                        </p>
-                    </div>
-                </Card>
-            </Modal>
             <BookingDetail open={isModalOpen} onCancel={handleCancel} />
-        </div>
+        </>
     );
 }
 
