878a6002e1aee1cce97ebba8f02e18a4912578d5
MinhKhoi01 <nguyenminhkhoi532@gmail.com>
2023-11-07 19:00:50 +0700

    [BE- user] minh khoi đang xử lý bình  luận đánh giá

--- ticketez_backend_springboot/src/main/java/com/ticketez_backend_springboot/modules/review/ReviewAPI.java
+++ ticketez_backend_springboot/src/main/java/com/ticketez_backend_springboot/modules/review/ReviewAPI.java
@@ -3,6 +3,8 @@ package com.ticketez_backend_springboot.modules.review;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,7 +16,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import com.ticketez_backend_springboot.modules.cinemaChain.CinemaChain;
+import com.ticketez_backend_springboot.modules.cinemaType.CinemaType;
 
 @CrossOrigin("*")
 @RestController
@@ -24,38 +27,63 @@ public class ReviewAPI {
     @Autowired
     ReviewDAO reviewDAO;
 
-    @GetMapping
-    public ResponseEntity<List<Review>> findAll( ) {
-        return ResponseEntity.ok(reviewDAO.findAll());
-    }
+    // @GetMapping
+    // public ResponseEntity<List<Review>> findAll() {
+    // return ResponseEntity.ok(reviewDAO.findAll());
+    // }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Review> findById(@PathVariable("id") Long id) {
-        if (!reviewDAO.existsById(id)) {
-            return ResponseEntity.notFound().build();
+    public ResponseEntity<?> findById(@PathVariable("id") Long id) {
+        try {
+            if (!reviewDAO.existsById(id)) {
+                return new ResponseEntity<>("Không tìm thấy review", HttpStatus.NOT_FOUND);
+            }
+            return ResponseEntity.ok(reviewDAO.findById(id).get());
+        } catch (Exception e) {
+            return new ResponseEntity<>("Server error, vui lòng thử lại sau!", HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return ResponseEntity.ok(reviewDAO.findById(id).get());
+
+    }
+
+    @GetMapping("/get/all")
+    public ResponseEntity<List<Review>> findAll() {
+        List<Review> review = reviewDAO.findAllByOrderByIdDesc();
+        return ResponseEntity.ok(review);
     }
 
     @PostMapping
-    public ResponseEntity<Review> post(@RequestBody Review review) {
-        reviewDAO.save(review);
-        return ResponseEntity.ok(review);
+    public ResponseEntity<?> post(@RequestBody Review review) {
+        try {
+            reviewDAO.save(review);
+            return ResponseEntity.ok(review);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Server error, vui lòng thử lại sau!", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Review> put(@PathVariable("id") Long id, @RequestBody Review review) {
-        if (!reviewDAO.existsById(id)) {
-            return ResponseEntity.notFound().build();
+    public ResponseEntity<?> put(@PathVariable("id") Long id, @RequestBody Review review) {
+        try {
+            if (!reviewDAO.existsById(id)) {
+                return new ResponseEntity<>("Rạp không tồn tại", HttpStatus.NOT_FOUND);
+            }
+            reviewDAO.save(review);
+            return ResponseEntity.ok(review);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Server error, vui lòng thử lại sau!", HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        reviewDAO.save(review);
-        return ResponseEntity.ok(review);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Boolean> delete(@PathVariable("id") Long id) {
-        reviewDAO.deleteById(id);
-        return ResponseEntity.ok(true);
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        try {
+            reviewDAO.deleteById(id);
+            return ResponseEntity.ok().body("Xoá rạp thành công");
+        } catch (DataIntegrityViolationException e) {
+            return new ResponseEntity<>("Không thể xóa", HttpStatus.CONFLICT);
+        }
+
     }
 
 }
