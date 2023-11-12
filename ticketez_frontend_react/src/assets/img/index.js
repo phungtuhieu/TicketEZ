a76e802ac46646d632d5148baca705145c53a94c
HuuTaiOne <nguyentaint979@gmail.com>
2023-11-12 16:28:15 +0700

    [FE - User], Hữu Tài - hoàn thành footer

--- ticketez_frontend_react/src/assets/img/index.js
+++ ticketez_frontend_react/src/assets/img/index.js
@@ -4,33 +4,59 @@ import datrungphuongnam from './datrungphuongnam.jpg';
 import tagDatTruoc from './logo-dat-truoc.svg';
 import event from './event.jpg';
 
-// 
+//
 import logoComment from './logo-cmt.webp';
-import notFoundLogo from './not-found.svg'
-import gifLoading from './loading.gif'
+import notFoundLogo from './not-found.svg';
+import gifLoading from './loading.gif';
 
-// 
-import markerPink from '../icon/mapbox-marker-icon-20px-pink.png'
+//
+import markerPink from '../icon/mapbox-marker-icon-20px-pink.png';
+
+import veXimPhim from '../icon/f-ve-xem-phim.svg';
+import baoHiem from '../icon/f-bao-hiem.svg';
+import veMayBay from '../icon/f-ve-may-bay.svg';
+import veKhachSan from '../icon/f-khach-san.svg';
+import viNhanAi from '../icon/f-vi-nhan-ai.webp';
+import vayNhanh from '../icon/f-vay-nhanh.webp';
+
+import faceBook from '../icon/f-facebook.svg';
+import linkedIn from '../icon/f-linkedIn.svg';
+import youtube from '../icon/f-youtube.svg';
+import daDangKy from '../icon/f-da-dang-ky.webp';
+
+import icon1 from '../icon/icon1.png';
+import icon2 from '../icon/icon2.png';
+import icon3 from '../icon/icon3.png';
+import momo from '../icon/momo.png';
 
 export const listIcon = {
-    markerPink
-}
-
-
+    markerPink,
+    veXimPhim,
+    baoHiem,
+    veMayBay,
+    veKhachSan,
+    viNhanAi,
+    vayNhanh,
+    faceBook,
+    linkedIn,
+    youtube,
+    daDangKy,
+    icon1,
+    icon2,
+    icon3,
+    momo,
+};
 
 const img = {
     logoLogin,
     bgAuth,
     datrungphuongnam,
     tagDatTruoc,
-    // 
+    //
     notFoundLogo,
     gifLoading,
     event,
-    logoComment
+    logoComment,
 };
 
-
 export default img;
-
-
--- ticketez_frontend_react/src/layouts/User/Footer/index.jsx
+++ ticketez_frontend_react/src/layouts/User/Footer/index.jsx
@@ -1,16 +1,248 @@
-const footerStyle = {
-    textAlign: 'center',
-    color: '#fff',
-    height: 64,
-    paddingInline: 50,
-    lineHeight: '64px',
-    backgroundColor: '#7dbcea',
-};
+import { Row, Col } from 'antd';
+import { listIcon } from '~/assets/img';
 function Footer() {
     return (
-        <div>
-            <div style={footerStyle}>Footer</div>
-        </div>
+        <Row>
+            <Col
+                span={24}
+                className="tw-bg-[#171717] tw-h-[450px] tw-w-[100%] tw-flex  tw-justify-center tw-px-[30px] tw-py-[15px]"
+            >
+                <Row className=" tw-w-[85%] tw-py-2 tw-px-[2rem]">
+                    <Col span={5} className="tw-text-[14px]">
+                        <h1 className="tw-text-[14px] tw-text-[#ffffffcc]">MUA VÉ XEM PHIM</h1>
+                        <div className=" tw-mb-4">
+                            <a href="#1">
+                                <span className="tw-text-[#ffffff80] tw-transition tw-duration-300 tw-ease-in-out hover:tw-text-[#D9D9D9]">
+                                    {' '}
+                                    Lịch chiếu phim
+                                </span>
+                            </a>
+                        </div>
+                        <div className="tw-mb-4">
+                            <a href="#1">
+                                <span className="tw-text-[#ffffff80] tw-transition tw-duration-300 tw-ease-in-out hover:tw-text-[#D9D9D9]">
+                                    {' '}
+                                    Rạp chiếu phim
+                                </span>
+                            </a>
+                        </div>
+                        <div className="tw-mb-4">
+                            <a href="#1">
+                                <span className="tw-text-[#ffffff80] tw-transition tw-duration-300 tw-ease-in-out hover:tw-text-[#D9D9D9]">
+                                    {' '}
+                                    Phim chiếu rạp
+                                </span>
+                            </a>
+                        </div>
+                        <div className="tw-mb-4">
+                            <a href="#1">
+                                <span className="tw-text-[#ffffff80] tw-transition tw-duration-300 tw-ease-in-out hover:tw-text-[#D9D9D9]">
+                                    {' '}
+                                    Review phim
+                                </span>
+                            </a>
+                        </div>
+                        <div className="tw-mb-4">
+                            <a href="#1">
+                                <span className="tw-text-[#ffffff80] tw-transition tw-duration-300 tw-ease-in-out hover:tw-text-[#D9D9D9]">
+                                    {' '}
+                                    Top phim
+                                </span>
+                            </a>
+                        </div>
+                        <div className="tw-mb-4">
+                            <a href="#1">
+                                <span className="tw-text-[#ffffff80] tw-transition tw-duration-300 tw-ease-in-out hover:tw-text-[#D9D9D9]">
+                                    {' '}
+                                    Blog phim
+                                </span>
+                            </a>
+                        </div>
+                    </Col>
+                    <Col span={5}>
+                        <h1 className="tw-text-[14px] tw-text-[#ffffffcc]">DỊCH VỤ NỔI BẬT</h1>
+                        <div className="tw-flex tw-items-center tw-mb-4  ">
+                            <a href="#1" className="tw-flex tw-items-center tw-group">
+                                <img src={listIcon.veXimPhim} alt="" width={24} className="tw-mr-3" />
+                                <span className="tw-text-[#ffffff80] tw-transition tw-duration-300 tw-ease-in-out group-hover:tw-text-[#D9D9D9]">
+                                    {' '}
+                                    Vé xem phim
+                                </span>
+                            </a>
+                        </div>
+                        <div className="tw-flex tw-items-center tw-mb-4 ">
+                            <a href="#1" className="tw-flex tw-items-center tw-group">
+                                <img src={listIcon.baoHiem} alt="" width={24} className="tw-mr-3" />
+                                <span className="tw-text-[#ffffff80] tw-transition tw-duration-300 tw-ease-in-out group-hover:tw-text-[#D9D9D9]">
+                                    {' '}
+                                    Bảo hiểm Ô tô
+                                </span>
+                            </a>
+                        </div>
+                        <div className="tw-flex tw-items-center tw-mb-4 ">
+                            <a href="#1" className="tw-flex tw-items-center tw-group">
+                                <img src={listIcon.veMayBay} alt="" width={24} className="tw-mr-3" />
+                                <span className="tw-text-[#ffffff80] tw-transition tw-duration-300 tw-ease-in-out group-hover:tw-text-[#D9D9D9]">
+                                    {' '}
+                                    Vé máy bay
+                                </span>
+                            </a>
+                        </div>
+                        <div className="tw-flex tw-items-center tw-mb-4 ">
+                            <a href="#1" className="tw-flex tw-items-center tw-group">
+                                <img src={listIcon.veKhachSan} alt="" width={24} className="tw-mr-3" />
+                                <span className="tw-text-[#ffffff80] tw-transition tw-duration-300 tw-ease-in-out group-hover:tw-text-[#D9D9D9]">
+                                    {' '}
+                                    Khách sạn
+                                </span>
+                            </a>
+                        </div>
+                        <div className="tw-flex tw-items-center tw-mb-4 ">
+                            <a href="#1" className="tw-flex tw-items-center tw-group">
+                                <img src={listIcon.viNhanAi} alt="" width={24} className="tw-mr-3" />
+                                <span className="tw-text-[#ffffff80] tw-transition tw-duration-300 tw-ease-in-out group-hover:tw-text-[#D9D9D9]">
+                                    {' '}
+                                    Ví nhân ái
+                                </span>
+                            </a>
+                        </div>
+                        <div className="tw-flex tw-items-center tw-mb-4 ">
+                            <a href="#1" className="tw-flex tw-items-center tw-group">
+                                <img src={listIcon.vayNhanh} alt="" width={24} className="tw-mr-3" />
+                                <span className="tw-text-[#ffffff80] tw-transition tw-duration-300 tw-ease-in-out group-hover:tw-text-[#D9D9D9]">
+                                    {' '}
+                                    Vay nhanh
+                                </span>
+                            </a>
+                        </div>
+                    </Col>
+                    <Col span={8} className="tw-text-[14px]">
+                        <h1 className="tw-text-[14px] tw-text-[#ffffffcc]">CHĂM SÓC KHÁCH HÀNG</h1>
+                        <div className="tw-mb-3">
+                            <span className="tw-text-[#8B8B8B]">Địa chỉ:</span>
+                            <span className="tw-text-[#9F9F9F] tw-transition tw-duration-300 tw-ease-in-out hover:tw-text-[#D9D9D9] ">
+                                Tầng M, Tòa nhà Victory Tower, Số 12 Tân Trào, Phường Tân Phú, Quận 7, Thành phố Hồ Chí
+                                Minh
+                            </span>
+                        </div>
+                        <div className="tw-mb-3">
+                            <span className="tw-text-[#8B8B8B]">Hotline : </span>
+                            <span>
+                                {' '}
+                                <a
+                                    href="#4"
+                                    className="tw-text-[#9F9F9F] tw-transition tw-duration-300 tw-ease-in-out hover:tw-text-[#D9D9D9]"
+                                >
+                                    {' '}
+                                    1900 5454 41
+                                </a>
+                            </span>
+                            <span>
+                                {' '}
+                                <i className="tw-text-[12px] tw-text-[#ffffff99]">(1000 đ/phút)</i>
+                            </span>
+                        </div>
+                        <div className="tw-mb-3">
+                            <span className="tw-text-[#8B8B8B]">Email :</span>
+                            <span className="tw-text-[#9F9F9F] tw-transition tw-duration-300 tw-ease-in-out hover:tw-text-[#D9D9D9]">
+                                {' '}
+                                hotro@momo.vn
+                            </span>
+                        </div>
+                        <div className="tw-mb-3 ">
+                            <span className="tw-text-[#8B8B8B]">Tổng đài gọi ra : </span>
+                            <span>
+                                <a
+                                    href="#2"
+                                    className="tw-text-[#9F9F9F] tw-transition tw-duration-300 tw-ease-in-out hover:tw-text-[#D9D9D9] after:tw-content-['_-']"
+                                >
+                                    028.7306.5555
+                                </a>{' '}
+                                <a
+                                    href="#4"
+                                    className="tw-text-[#9F9F9F] tw-transition tw-duration-300 tw-ease-in-out hover:tw-text-[#D9D9D9]"
+                                >
+                                    028.9999.5555
+                                </a>
+                            </span>
+                        </div>
+                        <div className="tw-mb-3">
+                            <img src={listIcon.icon2} alt="" width={180} height={55} />
+                        </div>
+
+                        <h1 className="tw-text-[14px] tw-text-[#ffffffcc] ">HỢP TÁC DOANH NGHIỆP</h1>
+                        <div className="tw-mb-3 ">
+                            <span className="tw-text-[#8B8B8B]"> Hotline :</span>
+                            <span>
+                                <a
+                                    href="#4"
+                                    className="tw-text-[#9F9F9F] tw-transition tw-duration-300 tw-ease-in-out hover:tw-text-[#D9D9D9]"
+                                >
+                                    1900 636 652 (Phí 1.000đ/phút)
+                                </a>
+                            </span>
+                        </div>
+                        <div className="tw-mb-3 ">
+                            <span className="tw-text-[#8B8B8B]">Email :</span>
+                            <span>
+                                <a
+                                    href="#4"
+                                    className="tw-text-[#9F9F9F] tw-transition tw-duration-300 tw-ease-in-out hover:tw-text-[#D9D9D9]"
+                                >
+                                    merchant.care@momo.vn
+                                </a>
+                            </span>
+                        </div>
+                        <div className="tw-mb-3 ">
+                            <span className="tw-text-[#8B8B8B]">Website :</span>
+                            <span>
+                                <a
+                                    href="#2"
+                                    className="tw-text-[#9F9F9F] tw-transition tw-duration-300 tw-ease-in-out hover:tw-text-[#D9D9D9]"
+                                >
+                                    business.momo.vn
+                                </a>
+                            </span>
+                        </div>
+                        <div className="tw-mb-3 ">
+                            <img src={listIcon.icon1} alt="" width={180} height={55} />
+                        </div>
+                    </Col>
+                    <Col span={6} className="tw-pl-[20px]">
+                        <h1 className="tw-text-[14px] tw-text-[#ffffffcc]">KẾT NỐI VỚI CHÚNG TÔI</h1>
+                        <div>
+                            <img src={listIcon.faceBook} alt="" width={40} className="tw-mr-[20px] tw-cursor-pointer " />
+                            <img src={listIcon.linkedIn} alt="" width={40} className="tw-mr-[20px] tw-cursor-pointer" />
+                            <img src={listIcon.youtube} alt="" width={40} className="tw-mr-[20px] tw-cursor-pointer" />
+                        </div>
+                        <h1 className="tw-text-[14px] tw-text-[#ffffffcc] tw-mt-[50px]">TẢI ỨNG DỤNG TRÊN ĐIỆN THOẠI</h1>
+                        <div className="">
+                            <img src={listIcon.icon3} alt="" width={230} height={110} />
+                        </div>
+                        <h1 className="tw-text-[14px] tw-text-[#ffffffcc] tw-mt-[50px]">ĐƯỢC CHỨNG NHẬN BỞI</h1>
+                        <div>
+                            <img src={listIcon.daDangKy} alt="" width={119} />
+                        </div>
+                    </Col>
+                </Row>
+            </Col>
+            <Col span={24} className="tw-bg-[#262626] tw-h-[80px] tw-w-[100%] tw-flex  tw-justify-center tw-px-[30px] tw-py-[10px]">
+                <Row className='tw-w-[85%] tw-py-[10px]'> 
+                    <Col span={1} className=''>
+                        <img src={listIcon.momo} alt="" width={50} />
+                    </Col>
+                    <Col span={15} className='tw-flex tw-items-end '>
+                        <div className='tw-mb-4'>
+                            <div className='tw-text-[16px] tw-text-[#CBCBCB]'>CÔNG TY CỔ PHẦN DỊCH VỤ DI ĐỘNG TRỰC TUYẾN</div>
+                            <div className='tw-text-[14px] tw-text-[#ffffff80]'>Lầu 6, Toà nhà Phú Mỹ Hưng, số 8 Hoàng Văn Thái, khu phố 1, P. Tân Phú, Q. 7, Thành phố Hồ Chí Minh</div>
+                        </div>
+                    </Col>
+                    <Col span={8} className='tw-flex tw-items-end tw-justify-end tw-mb-4 '>
+                        <div className='tw-text-[14px] tw-text-[#ffffff80] tw-pr-[75px]'>©Copyright M_Service 2023</div>
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
     );
 }
 
