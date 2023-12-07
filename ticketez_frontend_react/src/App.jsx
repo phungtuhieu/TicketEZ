7b2c63e2c57eda963f6f8cb7b2220217ef238093
Nhdinh <nhdinhpc03@gmail.com>
2023-12-07 19:23:42 +0700

    Hoàng Dinh sử lý đăng nhập bắt đường dẫn Routes

--- ticketez_frontend_react/src/App.jsx
+++ ticketez_frontend_react/src/App.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { publicRoutes } from '~/routes';
-import { DefaultLayout } from '~/layouts';
+import { privateRoutes, publicRoutes } from '~/routes';
+import { AdminLayout, DefaultLayout } from '~/layouts';
 import { Fragment } from 'react';
 import NotFound from './pages/NotFound/notFound';
 import { ToastContainer } from 'react-toastify';
+import { getRolesFromLocalStorage } from './utils/authUtils';
+
 function App() {
+    const roles = getRolesFromLocalStorage();
+    const isAuthenticated = roles && roles.length > 0;
     return (
         <Router>
             <div className="App">
@@ -16,6 +20,34 @@ function App() {
                             Layout = route.layout;
                         } else if (route.layout === null) {
                             Layout = Fragment;
+                        }
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                        <ToastContainer />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+
+                    {privateRoutes.map((route, index) => {
+                        if (route.requireRole === 'SUPER_ADMIN'
+                            && !roles.includes('MOVIE_MANAGEMENT_ADMIN')) {
+                            return null;
+                        }
+                        if (route.requireRole === ' SCHEDULING_PRICING_ADMIN'
+                            && !roles.includes(' USER_MANAGEMENT_ADMIN')) {
+                            return null;
+                        }
+                        const Page = route.component;
+                        let Layout = route.layout ? route.layout : Fragment;
+                        if (!isAuthenticated || !roles.includes(route.requireRole)) {
+                            return null;
                         }
                         return (
                             <Route
