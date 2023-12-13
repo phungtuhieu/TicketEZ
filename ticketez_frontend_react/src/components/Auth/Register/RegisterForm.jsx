a0bb6105a93ebf2e6d073b9e6fca2b1fe81c27da
Nhdinh <nhdinhpc03@gmail.com>
2023-12-13 17:19:16 +0700

    Hoàng Dinh sửa lại đăng ký thực hiện currentStep chuyển trang đăng ký

--- ticketez_frontend_react/src/components/Auth/Register/RegisterForm.jsx
+++ ticketez_frontend_react/src/components/Auth/Register/RegisterForm.jsx
@@ -6,7 +6,7 @@ import img from '~/assets/img';
 import authApi from '~/api/user/Security/authApi';
 import funcUtils from '~/utils/funcUtils';
 import backgroundImage from '~/assets/img/texure.jpg';
-import { validateEmail, validateId, validatePassword, validatePhone ,validateFullname} from '../Custom';
+import { validateEmail, validateId, validatePassword, validatePhone, validateFullname } from '../Custom';
 
 const { Header, Content } = Layout;
 
@@ -14,23 +14,22 @@ const { Header, Content } = Layout;
 const RegisterForm = () => {
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
+    const [currentStep, setCurrentStep] = useState(1);
+    const [form] = Form.useForm();
+    const [registrationData, setRegistrationData] = useState({});
 
-    const onFinish = async (values) => {
+    const onFinish = async () => {
         setLoading(true);
+        const finalData = {
+            ...registrationData,
+            ...form.getFieldsValue(),
+            gender: form.getFieldValue('gender') === 'male'
+        };
         try {
-            await authApi.signup({
-                id: values.id,
-                fullname: values.fullname,
-                email: values.email,
-                password: values.password,
-                phone: values.phone,
-                gender: values.gender === true,
-            });
-
-            funcUtils.notify('Đăng ký thành công!', 'success');
+            await authApi.signup(finalData);
+            funcUtils.notify('Đăng ký thành công kiểm tra email nhập mã để xác nhận tài khoản!', 'success');
             navigate('/otp');
         } catch (error) {
-            // Phân tích cú pháp lỗi từ phản hồi của API
             const errorMessage = error.response?.data?.message;
             funcUtils.notify(errorMessage, 'error');
         } finally {
@@ -39,153 +38,137 @@ const RegisterForm = () => {
     };
 
 
+
+    const onNext = async () => {
+        try {
+            const values = await form.validateFields(
+                currentStep === 1 ? ['id', 'fullname'] :
+                    currentStep === 2 ? ['phone', 'email'] :
+                        []);
+            setRegistrationData(prevData => ({ ...prevData, ...values }));
+            setCurrentStep(currentStep + 1);
+        } catch (errorInfo) {
+            console.log('Failed:', errorInfo);
+        }
+    };
+
+
+    const onPrevious = () => {
+        setCurrentStep(currentStep - 1);
+    };
     return (
-        <Layout className="layout" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover' }}>
+        <Layout className="layout">
             <Content className={styles.content}>
                 <div className={styles.wrapper}>
-                    <div className={styles.loginContainer}>
+                    <div className={styles.registerContainer}>
                         <h1 className={styles.title}>Đăng Ký</h1>
-                        {/* <p className={styles.welcomeText}>Chào mừng bạn đến giao diện người dùng TicketEZ Vui lòng nhập thông tin của bạn dưới đây để đăng nhập.</p> */}
                         <Form
-                            name="basic"
-                            className={styles.loginForm}
-                            initialValues={{ remember: true }}
+                            form={form}
+                            name="register"
+                            className={styles.registerForm}
                             onFinish={onFinish}
                             autoComplete="off"
                         >
-                            <div className='formLabel'>
-                                <p className={styles.formLabel}><h4>Tên Tài khoản:</h4></p>
-                            </div>
-                            <Form.Item
-                                // label="Tên tài khoản"
-                                name="id"
-                                rules={[
-                                    { validator: validateId }, 
-                                  ]}
-                                className={styles.formItem}
-                            >
-                                <Input placeholder="Tên Tài khoản" className={styles.input} />
-                            </Form.Item>
+                            {currentStep === 1 && (
+                                <>
+                                    <Form.Item
+                                        name="id"
 
-                            <div className='formLabel'>
-                                <p className={styles.formLabeFilename}><h4>Họ và tên:</h4></p>
-                            </div>
-                            <Form.Item
-                                // label="Tên tài khoản"
-                                name="fullname"
-                                rules={[
-                                    { validator: validateFullname }, 
-                                  ]}
-                                className={styles.formItem}
-                            >
-                                <Input placeholder="Họ và tên" className={styles.input} />
-                            </Form.Item>
-                            <div className='formLabel'>
-                                <p className={styles.formLabelPhone}><h4>Số điện thoại:</h4></p>
-                            </div>
-                            <Form.Item
-                                name="phone"
-                                rules={[
-                                    { validator: validatePhone }, 
-                                  ]}
-                                className={styles.formItem}
-                            >
-                                <Input placeholder="Số điện thoại" className={styles.input} />
-                            </Form.Item>
+                                        rules={[{
+                                            validator: validateId
+                                        }]}
+                                    >
+                                        <Input placeholder="Tên Tài khoản" />
+                                    </Form.Item>
+                                    <Form.Item
+                                        name="fullname"
+                                        rules={[{ validator: validateFullname }]}
+                                    >
+                                        <Input placeholder="Họ và tên" />
+                                    </Form.Item>
+                                    <Button type="primary" onClick={onNext}>
+                                        Tiếp theo
+                                    </Button>
+                                </>
+                            )}
 
-                            <div className='formLabel'>
-                                <p className={styles.formLabeEmail}><h4>Email:</h4></p>
-                            </div>
-                            <Form.Item
-                                name="email"
-                                rules={[
-                                    { validator: validateEmail }, 
-                                  ]}
-                                className={styles.formItem}
-                            >
-                                <Input placeholder="Email" className={styles.input} />
-                            </Form.Item>
-                            <Form.Item
-                                name="gender"
-                                label="Giới tính"
-                                rules={[{ required: true, message: 'Vui lòng chọn giới tính!' }]}
-                            >
-                                <Radio.Group>
-                                    <Radio value="male">Nam</Radio>
-                                    <Radio value="female">Nữ</Radio>
-                                </Radio.Group>
-                            </Form.Item>
-                            <div className='formLabel'>
-                                <p className={styles.formLabelPassword}><h4>Mật khẩu:</h4></p>
-                            </div>
-                            <Form.Item
-                                // label="Mật khẩu"
-                                name="password"
-                                rules={[
-                                    { validator: validatePassword }, 
-                                  ]}
-                                className={styles.formItem}
-                            >
-                                <Input.Password type="password" placeholder="Mật khẩu" className={styles.input} />
+                            {currentStep === 2 && (
+                                <>
+                                    <Form.Item
+                                        name="phone"
+                                        rules={[{ validator: validatePhone }]}
+                                    >
+                                        <Input placeholder="Số điện thoại" />
+                                    </Form.Item>
+                                    <Form.Item
+                                        name="email"
+                                        rules={[{ validator: validateEmail }]}
+                                    >
+                                        <Input placeholder="Email" />
+                                    </Form.Item>
+                                    <div className="button-group">
+                                        <Button onClick={onPrevious} style={{ marginRight: '20px' }}>
+                                            Quay lại
+                                        </Button>
 
-                            </Form.Item>
+                                        <Button type="primary" onClick={onNext}>
+                                            Tiếp theo
+                                        </Button>
+                                    </div>
 
-                            <div className='formLabel'>
-                                <p className={styles.confirm}><h4>Xác nhận mật khẩu:</h4></p>
-                            </div>
-                            <Form.Item
+                                </>
+                            )}
 
-                                name="confirm"
-                                dependencies={['password']}
-                                hasFeedback
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Vui lòng xác nhận mật khẩu của bạn!',
-                                    },
-                                    ({ getFieldValue }) => ({
-                                        validator(_, value) {
-                                            if (!value || getFieldValue('password') === value) {
-                                                return Promise.resolve();
-                                            }
-                                            return Promise.reject(new Error('Hai mật khẩu bạn nhập không khớp !'));
-                                        },
-                                    }),
-                                ]}
-                            >
-                                <Input.Password type="password" placeholder="Xác nhận mật khẩu" className={styles.input} />
-                            </Form.Item>
-
-                            <Form.Item
-                                name="agreement"
-                                valuePropName="checked"
-                                rules={[
-                                    {
-                                        validator: (_, value) =>
-                                            value ? Promise.resolve() : Promise.reject(new Error('Bạn phải đồng ý với các điều khoản sử dụng')),
-                                    },
-                                ]}
-                                wrapperCol={{ span: 24 }}
-                            >
-                                <Checkbox>
-                                    Tôi đã đọc và đồng ý với <a href="">điều khoản sử dụng</a>.
-                                </Checkbox>
-                            </Form.Item>
-
-                            <Form.Item className={styles.formItem}>
-                                <Button type="primary" htmlType="submit" block className={styles.loginButton} loading={loading}>
-                                    Đăng ký
-                                </Button>
-                                <p className={styles.signup}>
-                                    Đăng nhập tại <a href="http://localhost:3000/Login">Đăng Nhập</a>
-                                </p>
-                            </Form.Item>
-
+                            {currentStep === 3 && (
+                                <>
+                                    <Form.Item
+                                        name="gender"
+                                        rules={[{ required: true, message: 'Vui lòng chọn giới tính!' }]}
+                                    >
+                                        <Radio.Group>
+                                            <Radio value="male">Nam</Radio>
+                                            <Radio value="female">Nữ</Radio>
+                                        </Radio.Group>
+                                    </Form.Item>
+                                    <Form.Item
+                                        name="password"
+                                        rules={[{ validator: validatePassword }]}
+                                    >
+                                        <Input.Password placeholder="Mật khẩu" />
+                                    </Form.Item>
+                                    <Form.Item
+                                        name="confirm"
+                                        dependencies={['password']}
+                                        hasFeedback
+                                        rules={[
+                                            {
+                                                required: true,
+                                                message: 'Vui lòng xác nhận mật khẩu của bạn!',
+                                            },
+                                            ({ getFieldValue }) => ({
+                                                validator(_, value) {
+                                                    if (!value || getFieldValue('password') === value) {
+                                                        return Promise.resolve();
+                                                    }
+                                                    return Promise.reject(new Error('Hai mật khẩu bạn nhập không khớp!'));
+                                                },
+                                            }),
+                                        ]}
+                                    >
+                                        <Input.Password placeholder="Xác nhận mật khẩu" />
+                                    </Form.Item>
+                                    <Button onClick={onPrevious} style={{ marginRight: '20px' }}>
+                                        Quay lại
+                                    </Button>
+                                    <Button type="primary" htmlType="submit" loading={loading}>
+                                        Đăng ký
+                                    </Button>
+                                </>
+                            )}
                         </Form>
                     </div>
                 </div>
             </Content>
-
         </Layout>
     );
 };
